9da7525db9e4136f876234db9c545a3ae9ee94cc
AidDeath <aiddeath@gmail.com>
2022-09-23 12:11:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+
+namespace Prank1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ShowWindow(Process.GetCurrentProcess().MainWindowHandle, 0);
+            var lastPosition = GetCursorPosition();
+            while (true)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                if (lastPosition != GetCursorPosition())
+                {
+                    Console.Beep();
+					Beep(2000, 400);
+                    lastPosition = GetCursorPosition();
+
+                }
+                
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MousePosition
+        {
+            public int X;
+            public int Y;
+
+            public static implicit operator Point(MousePosition Position)
+            {
+                return new Point(Position.X, Position.Y);
+            }
+        }
+
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out MousePosition position);
+
+        public static Point GetCursorPosition()
+        {
+            GetCursorPos(out var mousePosition);
+            return mousePosition;
+        }
+
+        //Console Window Manage
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+
+        [DllImport("kernel32.dll")]
+        public static extern bool Beep(int freq, int duration);
+
+    }
+}
